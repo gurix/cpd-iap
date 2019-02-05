0ac5025b315287a90dc96c9c1dfb07ea806b3936
Markus Graf <info@markusgraf.ch>
2019-02-05 15:02:24 +0100

    Make sure the user touched each slider at least.

--- app/models/survey/session_rating_scale.rb
+++ app/models/survey/session_rating_scale.rb
@@ -9,10 +9,10 @@ module Survey
     validates :version, presence: true
 
     # The real survey fields
-    field :relationship,       type: Integer, default: 50
-    field :goals_and_topics,   type: Integer, default: 50
-    field :approach_or_method, type: Integer, default: 50
-    field :overall,            type: Integer, default: 50
+    field :relationship,       type: Integer
+    field :goals_and_topics,   type: Integer
+    field :approach_or_method, type: Integer
+    field :overall,            type: Integer
 
     validates :relationship,       presence: true
     validates :goals_and_topics,   presence: true
--- spec/features/client_session_rating_scale_spec.rb
+++ spec/features/client_session_rating_scale_spec.rb
@@ -1,6 +1,12 @@
 require 'rails_helper'
 
-feature 'session rating scale input' do
+def range_select(name, value)
+  selector = %(input[type=range][name=\\"#{name}\\"])
+  script = %-$("#{selector}").val(#{value}).trigger('click')-
+  page.execute_script(script)
+end
+
+feature 'session rating scale input', js: true do
   scenario 'a new client fills a survey' do
     Counselor.create(name: 'Sigmund Freud', email: 'sigmund@sigmund-freud.at')
     Counselor.create(name: 'Dr. Paul Weston', email: 'paul.weston@intreatment.movie')
@@ -27,14 +33,23 @@ feature 'session rating scale input' do
 
     expect(current_path).to eq new_client_survey_session_rating_scale_path(Client.last)
 
-    find("input[id$='survey_session_rating_scale_relationship']").set 10
-    find("input[id$='survey_session_rating_scale_goals_and_topics']").set 20
-    find("input[id$='survey_session_rating_scale_approach_or_method']").set 10
-    find("input[id$='survey_session_rating_scale_overall']").set 20
+    range_select('relationship', 10)
+    range_select('goals_and_topics', 20)
+
+    click_button 'Abschliessen'
+    expect(page).to have_content 'muss ausgefüllt werden'
+
+    range_select('approach_or_method', 30)
+    range_select('overall', 40)
 
     expect { click_button 'Abschliessen' }.to change { ActionMailer::Base.deliveries.count }.by(1)
 
     session = Survey::Session.desc(:created_at).last
+
+    expect(session.relationship).to eq 10
+    expect(session.goals_and_topics).to eq 20
+    expect(session.approach_or_method).to eq 30
+    expect(session.overall).to eq 40
 
     expect(ActionMailer::Base.deliveries.last.body).to have_content new_client_survey_session_rating_scale_counselor_rating_url(session.client, session)
 
